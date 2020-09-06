f4880d5fb3fc3d15bf0bb40cc99296895343b113
Rashmi Gupta <rashmi224141@gmail.com>
2020-09-07 02:08:50 +0530

    first build

---
+++ build/precache-manifest.354102a6ccc6d984a52724bb6a703038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ba03f2c83ee765c3ca07e4ce953478",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8072260ea8b8cf11ab0e",
+    "url": "/static/css/main.7ba61634.chunk.css"
+  },
+  {
+    "revision": "795bf742a1380017caeb",
+    "url": "/static/js/2.e3dbf7b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3dbf7b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8072260ea8b8cf11ab0e",
+    "url": "/static/js/main.8a4f526f.chunk.js"
+  },
+  {
+    "revision": "63e9e5a6aa0fb5c125c9",
+    "url": "/static/js/runtime-main.96387fa5.js"
+  }
+]);
