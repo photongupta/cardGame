160cd9b1a19236baa9bf663bf5cddc7a7274fbad
Rashmi Gupta <rashmi224141@gmail.com>
2020-09-07 02:06:54 +0530

    Modified header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Guess and match the cards</h1>
+      <h1>Guess The Matching Cards</h1>
       <Game cards={getCards(23)} />
     </div>
   );
